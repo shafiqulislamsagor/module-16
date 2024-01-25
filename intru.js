20593ffcd9532d36d35649e5355e534293afcfb8
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-26 03:29:33 +0600

    task 3

--- intru.js
+++ intru.js
@@ -11,3 +11,9 @@ var allSubjectMark = 75.25 + 65 + 80 + 35.45 + 99.50 ;
 var average = allSubjectMark / 5 ;
 
 console.log(average.toFixed(2));
+
+// Task 3
+var given = 119 ;
+var reminderNumber = 119 % 5 ;
+
+console.log(reminderNumber);
