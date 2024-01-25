c5a174199079d3b690aa2eb8e71e18e27eb44550
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-26 03:18:55 +0600

    task 2

--- intru.js
+++ intru.js
@@ -7,3 +7,7 @@ var moneyReturn = myMoney - buyFruits;
 console.log(moneyReturn);
 
 // Task 2
+var allSubjectMark = 75.25 + 65 + 80 + 35.45 + 99.50 ;
+var average = allSubjectMark / 5 ;
+
+console.log(average.toFixed(2));
